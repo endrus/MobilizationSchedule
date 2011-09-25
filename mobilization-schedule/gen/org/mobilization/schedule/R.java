9a5e60c24f9287a22214a1d9508ef516952cefe2
Andrzej Stasiak <endrusa@gmail.com>
2011-09-25 11:52:51 +0200

    adding Progress dialog when updating

--- mobilization-schedule/gen/org/mobilization/schedule/R.java
+++ mobilization-schedule/gen/org/mobilization/schedule/R.java
@@ -8,9 +8,6 @@
 package org.mobilization.schedule;
 
 public final class R {
-    public static final class array {
-        public static final int array=0x7f060000;
-    }
     public static final class attr {
     }
     public static final class color {
@@ -21,43 +18,53 @@ public final class R {
         public static final int details_bg=0x7f020000;
         public static final int icon=0x7f020001;
         public static final int logo=0x7f020002;
+        public static final int logo_150=0x7f020003;
+        public static final int logo_50=0x7f020004;
     }
     public static final class id {
-        public static final int big_hall_button=0x7f090008;
-        public static final int credits=0x7f09000b;
-        public static final int details=0x7f090006;
-        public static final int layout_root=0x7f090000;
-        public static final int linearLayout1=0x7f090003;
-        public static final int logo=0x7f090002;
-        public static final int small_hall_button=0x7f090009;
-        public static final int speaker=0x7f090005;
-        public static final int tableLayout1=0x7f090007;
-        public static final int textView1=0x7f090001;
-        public static final int update_schedule=0x7f09000a;
-        public static final int when=0x7f090004;
+        public static final int big_hall_button=0x7f080008;
+        public static final int credits=0x7f08000d;
+        public static final int details=0x7f080006;
+        public static final int imageView1=0x7f08000b;
+        public static final int layout_root=0x7f080000;
+        public static final int linearLayout1=0x7f080003;
+        public static final int logo=0x7f080002;
+        public static final int notify=0x7f08000a;
+        public static final int small_hall_button=0x7f080009;
+        public static final int speaker=0x7f080005;
+        public static final int tableLayout1=0x7f080007;
+        public static final int textView1=0x7f080001;
+        public static final int update_schedule=0x7f08000c;
+        public static final int when=0x7f080004;
     }
     public static final class layout {
         public static final int credits_dialog=0x7f030000;
         public static final int details=0x7f030001;
         public static final int event_row=0x7f030002;
         public static final int main=0x7f030003;
+        public static final int speaker_details=0x7f030004;
     }
     public static final class menu {
-        public static final int main_menu=0x7f080000;
+        public static final int main_menu=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f050002;
         public static final int big_hall=0x7f050001;
-        public static final int credits=0x7f050004;
-        public static final int no_schedule=0x7f050005;
+        public static final int credits=0x7f050006;
+        public static final int no_schedule=0x7f050007;
+        public static final int please_update=0x7f050008;
+        public static final int schedule_details=0x7f050003;
         public static final int small_hall=0x7f050000;
-        public static final int successfully_updated=0x7f050006;
-        public static final int update_failed=0x7f050007;
-        public static final int update_schedule=0x7f050003;
+        public static final int speeker_details=0x7f050004;
+        public static final int successfully_updated=0x7f050009;
+        public static final int update_failed=0x7f05000a;
+        public static final int update_schedule=0x7f050005;
+        public static final int updating=0x7f05000b;
+        public static final int updating_please_wait=0x7f05000c;
     }
     public static final class style {
-        public static final int button_pressed=0x7f070000;
-        public static final int button_releassed=0x7f070001;
-        public static final int text_black_font=0x7f070002;
+        public static final int button_pressed=0x7f060000;
+        public static final int button_releassed=0x7f060001;
+        public static final int text_black_font=0x7f060002;
     }
 }
---
+++ mobilization-schedule/src/main/java/org/mobilization/schedule/SpeakerDetailsAvtivity.java
@@ -0,0 +1,7 @@
+package org.mobilization.schedule;
+
+import android.app.Activity;
+
+public class SpeakerDetailsAvtivity extends Activity {
+
+}
