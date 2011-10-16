a4d62ea4dcdbbb7541828cddcf00b87bf3c5b575
Andrzej Stasiak <endrusa@gmail.com>
2011-10-16 19:27:51 +0200

    Removing last redundant files plus version upgrade

--- mobilization-schedule/gen/org/mobilization/schedule/R.java
+++ mobilization-schedule/gen/org/mobilization/schedule/R.java
@@ -19,17 +19,13 @@ public final class R {
         public static final int details_bg=0x7f020001;
         public static final int icon=0x7f020002;
         public static final int logo=0x7f020003;
-        public static final int logo_150=0x7f020004;
-        public static final int logo_50=0x7f020005;
-        public static final int quicklink=0x7f020006;
-        public static final int quicklink_orginal=0x7f020007;
+        public static final int logo_50=0x7f020004;
     }
     public static final class id {
         public static final int big_hall_button=0x7f08000b;
-        public static final int credits=0x7f080011;
+        public static final int credits=0x7f080010;
         public static final int details=0x7f080009;
         public static final int hours=0x7f08000a;
-        public static final int imageView1=0x7f08000e;
         public static final int layout_root=0x7f080000;
         public static final int linearLayout1=0x7f080006;
         public static final int logo=0x7f080005;
@@ -40,16 +36,15 @@ public final class R {
         public static final int tableRow1=0x7f080002;
         public static final int tableRow2=0x7f080003;
         public static final int tableRow3=0x7f080004;
-        public static final int update_schedule=0x7f08000f;
+        public static final int update_schedule=0x7f08000e;
         public static final int when=0x7f080007;
-        public static final int where=0x7f080010;
+        public static final int where=0x7f08000f;
     }
     public static final class layout {
         public static final int credits_dialog=0x7f030000;
         public static final int details=0x7f030001;
         public static final int event_row=0x7f030002;
         public static final int main=0x7f030003;
-        public static final int speaker_details=0x7f030004;
     }
     public static final class menu {
         public static final int main_menu=0x7f070000;
